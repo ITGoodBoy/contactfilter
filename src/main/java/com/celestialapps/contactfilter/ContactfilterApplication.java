d067dd74e0a4615f3ffc4b00c38b5b795b2cea41
ITGoodBoy <Celestialapps@Tutanota.com>
2018-09-03 02:01:09 +0300

    -update

--- src/main/java/com/celestialapps/contactfilter/ContactfilterApplication.java
+++ src/main/java/com/celestialapps/contactfilter/ContactfilterApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactfilterApplication {
 
-
-
-
-
     public static void main(String[] args) {
 		SpringApplication.run(ContactfilterApplication.class, args);
 	}
