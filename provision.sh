d0684c3b87c34a833c96d0fbc7f0256bb625f7ce
ITGoodBoy <Celestialapps@Tutanota.com>
2018-09-03 01:37:28 +0300

    -update

--- provision.sh
+++ provision.sh
@@ -14,7 +14,7 @@ sudo add-apt-repository ppa:linuxuprising/java
 sudo apt-get update
 sudo apt-get install -y oracle-java10-installer
 sudo apt-get install -y oracle-java10-set-default
-sudo useradd -p 11111111 faster
-sudo su faster
 cd /vagrant
-mvn -s setting.xml clean install
+mvn clean package
+cd target
+java -jar contactfilter-0.0.1-SNAPSHOT.jar
